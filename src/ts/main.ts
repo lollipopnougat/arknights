906a56efbb70d29544e25894c13a8a36714d1d8c
lollipopnougat <ab2defg145@gmail.com>
2020-06-19 17:49:14 +0800

    早露池

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,10 @@
 import "../css/style.css";
 var $ = require('jquery');
-const six = ['能天使', '推进之王', '伊芙利特', '艾雅法拉', '安洁莉娜', '闪灵', '夜莺', '星熊', '塞雷娅', '银灰', '斯卡蒂', '陈', '黑', '赫拉格', '麦哲伦', '莫斯提马', '煌', '阿', '刻俄柏', '风笛', '傀影', '温蒂'];
+const six = ['能天使', '推进之王', '伊芙利特', '艾雅法拉', '安洁莉娜', '闪灵', '夜莺', '星熊', '塞雷娅', '银灰', '斯卡蒂', '陈', '黑', '赫拉格', '麦哲伦', '莫斯提马', '煌', '阿', '刻俄柏', '风笛', '傀影', '温蒂', '早露'];
 
-const five = ['白面鸮', '凛冬', '德克萨斯', '芙兰卡', '拉普兰德', '幽灵鲨', '蓝毒', '白金', '陨星', '天火', '梅尔', '赫默', '华法琳', '临光', '红', '雷蛇', '可颂', '普罗旺斯', '守林人', '崖心', '初雪', '真理', '空', '狮蝎', '食铁兽', '夜魔', '诗怀雅', '格劳克斯', '星极', '送葬人', '槐琥', '苇草', '布洛卡', '灰喉', '吽', '惊蛰', '慑砂', '巫恋', '极境', '月禾', '石棉'];
+const five = ['白面鸮', '凛冬', '德克萨斯', '芙兰卡', '拉普兰德', '幽灵鲨', '蓝毒', '白金', '陨星', '天火', '梅尔', '赫默', '华法琳', '临光', '红', '雷蛇', '可颂', '普罗旺斯', '守林人', '崖心', '初雪', '真理', '空', '狮蝎', '食铁兽', '夜魔', '诗怀雅', '格劳克斯', '星极', '送葬人', '槐琥', '苇草', '布洛卡', '灰喉', '吽', '惊蛰', '慑砂', '巫恋', '极境', '月禾', '石棉', '莱恩哈特'];
 
-const four = ['夜烟', '远山', '杰西卡', '流星', '白雪', '清道夫', '红豆', '杜宾', '缠丸', '霜叶', '慕斯', '砾', '暗索', '末药', '调香师', '角峰', '蛇屠箱', '古米', '深海色', '地灵', '阿消', '猎蜂', '格雷伊', '苏苏洛', '桃金娘', '红云', '梅', '安比尔', '宴', '刻刀'];
+const four = ['夜烟', '远山', '杰西卡', '流星', '白雪', '清道夫', '红豆', '杜宾', '缠丸', '霜叶', '慕斯', '砾', '暗索', '末药', '调香师', '角峰', '蛇屠箱', '古米', '深海色', '地灵', '阿消', '猎蜂', '格雷伊', '苏苏洛', '桃金娘', '红云', '梅', '安比尔', '宴', '刻刀', '波登可'];
 
 const three = ['芬', '香草', '翎羽', '玫兰莎', '卡缇', '米格鲁', '克洛丝', '炎熔', '芙蓉', '安塞尔', '史都华德', '梓兰', '空爆', '月见夜', '斑点', '泡普卡'];
 
@@ -32,17 +32,17 @@ const act1AgentLevel = {
 
 const act0Title = '常驻标准寻访';
 
-const act1Title = '雾漫荒林';
+const act1Title = '雪落晨心';
 
 const sixActivity0 = ['麦哲伦', '星熊']; // 活动卡池
 
 const fiveActivity0 = ['白面鸮', '德克萨斯', '真理'];
 
-const sixActivity1: string[] = [];
+const sixActivity1: string[] = ['早露'];
 
-const fiveActivity1 = ['石棉', '月禾'];
+const fiveActivity1 = ['莱恩哈特'];
 
-const fourActivity1: string[] = [];
+const fourActivity1: string[] = ['波登可'];
 
 
 var player: JQuery<HTMLAudioElement>;
@@ -198,7 +198,7 @@ class FindAgent {
         sixActProb: number = 50, sixActAgentsArray: string[] = [],
         fiveActProb = 50, fiveActAgentsArray: string[] = [],
         fourActProb: number = 50, fourActAgentsArray: string[] = [],
-        threeActProb: number = 50, threeActAgentsArray: string[] = [], ) {
+        threeActProb: number = 50, threeActAgentsArray: string[] = [],) {
         this.validDrawTimes = 0;
         this.isActivity = isActivity;
 
@@ -577,7 +577,7 @@ function RadioChange() {
         findAgent = new FindAgent(true, 50, sixActivity0, 50, fiveActivity0);
     }
     else if ($('#rad1').prop('checked')) {
-        findAgent = new FindAgent(true, 0, sixActivity1, 50, fiveActivity1);
+        findAgent = new FindAgent(true, 50, sixActivity1, 50, fiveActivity1, 50, fourActivity1);
     }
 }
 
@@ -591,8 +591,8 @@ window.onload = function () {
     }
     if ($('#rad1')) {
         $('#rad1').on('change', RadioChange);
-        //let text = act1Title + '(' + sixActivity1.join('、') + '、' + fiveActivity1.join('、') + '、' + fourActivity1.join('、') + ')';
-        let text = act1Title + '(' + sixActivity1.join('、') + '' + fiveActivity1.join('、') + ')';
+        let text = act1Title + '(' + sixActivity1.join('、') + '、' + fiveActivity1.join('、') + '、' + fourActivity1.join('、') + ')';
+        //let text = act1Title + '(' + sixActivity1.join('、') + '' + fiveActivity1.join('、') + ')';
         $('#act1').text(text);
     }
     for (let i = 0; i < 6; i++) {
