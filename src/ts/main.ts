7c2655ff2a6e91c2b3d806e6c91ae2bad4ec86c0
lollipopnougat <ab2defg145@gmail.com>
2021-03-17 15:04:38 +0800

    更新卡池

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,10 @@
 import "../css/style.css";
 var $ = require('jquery');
-const six = ['能天使', '推进之王', '伊芙利特', '艾雅法拉', '安洁莉娜', '闪灵', '夜莺', '星熊', '塞雷娅', '银灰', '斯卡蒂', '陈', '黑', '赫拉格', '麦哲伦', '莫斯提马', '煌', '阿', '刻俄柏', '风笛', '傀影', '温蒂', '早露', '铃兰', '棘刺', '森蚺', '史尔特尔', '瑕光', '泥岩', '山'];
+const six = ['能天使', '推进之王', '伊芙利特', '艾雅法拉', '安洁莉娜', '闪灵', '夜莺', '星熊', '塞雷娅', '银灰', '斯卡蒂', '陈', '黑', '赫拉格', '麦哲伦', '莫斯提马', '煌', '阿', '刻俄柏', '风笛', '傀影', '温蒂', '早露', '铃兰', '棘刺', '森蚺', '史尔特尔', '瑕光', '泥岩', '山', '空弦', '嵯峨'];
 
-const five = ['白面鸮', '凛冬', '德克萨斯', '芙兰卡', '拉普兰德', '幽灵鲨', '蓝毒', '白金', '陨星', '天火', '梅尔', '赫默', '华法琳', '临光', '红', '雷蛇', '可颂', '普罗旺斯', '守林人', '崖心', '初雪', '真理', '空', '狮蝎', '食铁兽', '夜魔', '诗怀雅', '格劳克斯', '星极', '送葬人', '槐琥', '苇草', '布洛卡', '灰喉', '吽', '惊蛰', '慑砂', '巫恋', '极境', '月禾', '石棉', '莱恩哈特', '蜜蜡', '贾维', '安哲拉', '燧石', '四月', '奥斯塔', '絮雨', '卡夫卡'];
+const five = ['白面鸮', '凛冬', '德克萨斯', '芙兰卡', '拉普兰德', '幽灵鲨', '蓝毒', '白金', '陨星', '天火', '梅尔', '赫默', '华法琳', '临光', '红', '雷蛇', '可颂', '普罗旺斯', '守林人', '崖心', '初雪', '真理', '空', '狮蝎', '食铁兽', '夜魔', '诗怀雅', '格劳克斯', '星极', '送葬人', '槐琥', '苇草', '布洛卡', '灰喉', '吽', '惊蛰', '慑砂', '巫恋', '极境', '月禾', '石棉', '莱恩哈特', '蜜蜡', '贾维', '安哲拉', '燧石', '四月', '奥斯塔', '絮雨', '卡夫卡', '爱丽丝', '乌有'];
 
-const four = ['夜烟', '远山', '杰西卡', '流星', '白雪', '清道夫', '红豆', '杜宾', '缠丸', '霜叶', '慕斯', '砾', '暗索', '末药', '调香师', '角峰', '蛇屠箱', '古米', '深海色', '地灵', '阿消', '猎蜂', '格雷伊', '苏苏洛', '桃金娘', '红云', '梅', '安比尔', '宴', '刻刀', '波登可', '卡达', '孑', '酸糖', '芳汀', '泡泡', '杰克', '松果'];
+const four = ['夜烟', '远山', '杰西卡', '流星', '白雪', '清道夫', '红豆', '杜宾', '缠丸', '霜叶', '慕斯', '砾', '暗索', '末药', '调香师', '角峰', '蛇屠箱', '古米', '深海色', '地灵', '阿消', '猎蜂', '格雷伊', '苏苏洛', '桃金娘', '红云', '梅', '安比尔', '宴', '刻刀', '波登可', '卡达', '孑', '酸糖', '芳汀', '泡泡', '杰克', '松果', '豆苗'];
 
 const three = ['芬', '香草', '翎羽', '玫兰莎', '卡缇', '米格鲁', '克洛丝', '炎熔', '芙蓉', '安塞尔', '史都华德', '梓兰', '空爆', '月见夜', '斑点', '泡普卡'];
 
@@ -20,21 +20,21 @@ const activityNum = 2;
 
 const act0Title = '常驻标准寻访';
 
-const act1Title = '月隐晦明'; // 活动卡池
+const act1Title = '进攻-防守-战术交汇'; // 活动卡池
 
 const act2Title = '锁与匙的守卫者';
 
-const sixActivity0: string[] = ['伊芙利特', '森蚺'];
+const sixActivity0: string[] = ['刻俄柏', '安洁莉娜'];
 
-const fiveActivity0: string[] = ['幽灵鲨', '惊蛰', '食铁兽'];
+const fiveActivity0: string[] = ['拉普兰德', '絮雨', '普罗旺斯'];
 
 const fourActivity0: string[] = [];
 
 const threeActivity0: string[] = [];
 
-const sixActivity1: string[] = ['夕', '嵯峨', '年'];
+const sixActivity1: string[] = ['灰烬'];
 
-const fiveActivity1: string[] = ['乌有'];
+const fiveActivity1: string[] = ['闪击', '霜华'];
 
 const fourActivity1: string[] = [];
 
@@ -60,7 +60,8 @@ const act0Prob: HuntProb = {
     six: 50,
     five: 50,
     four: 0,
-    three: 0
+    three: 0,
+    back: 0
 }
 
 const act1: Agents = {
@@ -81,14 +82,16 @@ const act1Prob: HuntProb = {
     six: 70,
     five: 50,
     four: 0,
-    three: 0
+    three: 0,
+    back: 120
 }
 
 const act2Prob: HuntProb = {
     six: 50,
     five: 50,
     four: 20,
-    three: 0
+    three: 0,
+    back: 0
 }
 
 //var player: JQuery<HTMLAudioElement>;
@@ -374,6 +377,7 @@ interface HuntProb {
     five: number;
     four: number;
     three: number;
+    back: number;
 }
 
 interface HuntRes {
@@ -446,13 +450,13 @@ class HeadHunter {
 
     public HuntOnce = (): HuntRes => {
         this.validSixTimes++;
-
+        
         let prob = this.rand.Next(0, 100);
-
+        
         let currSixProb = this.AddSixProb() % 100;
         let currFiveProb = (this.AddSixProb() + 8) % 100;
         let currFourProb = (this.AddSixProb() + 8 + 50) % 100;
-
+        
         if (this.minFloor > 1) this.minFloor--;
         else if (this.minFloor == 1) {
             if (this.rand.Next(0, 3) == 3) currSixProb = 100;
@@ -461,10 +465,21 @@ class HeadHunter {
                 currFiveProb = 100;
             }
         }
+
+        if (this.huntProb.back == this.validSixTimes) {
+            currSixProb = 100;
+        }
+
         let res: HuntRes;
 
         if (prob <= currSixProb) {
             this.minFloor = 0;
+            if (this.huntProb.back == this.validSixTimes) {
+                return {
+                    star: 6,
+                    name: this.activityAgents.six[0]
+                };
+            }
             //console.log('抽6');
             let res_name = this.GetAgent(6, this.isActivity && this.rand.Next(0, 100) <= this.huntProb.six && this.activityAgents.six.length != 0);
             this.validSixTimes = 0;
